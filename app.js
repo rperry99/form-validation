68e9dde8d82ad84ffdfa41f351504311a57622a1
Russ Perry <rperry1886@gmail.com>
2020-08-18 22:55:43 -0400

    Add storing info function
    This application doesnt do anything other than save the users entered info into an object and display the object to the console. Nothing actually gets saved.

--- app.js
+++ app.js
@@ -5,17 +5,55 @@ const password2El = document.getElementById('password2');
 const messageContainer = document.querySelector('.message-container');
 const message = document.getElementById('message');
 
-// Validate form
 let isValid = false;
+let passwordsMatch = false;
 
 function validateForm() {
   // USing Constraing API
   isValid = form.checkValidity();
 
   // Style main message for an error
-  message.textContent = 'Please fill out all fields.';
-  message.style.color = 'red';
-  messageContainer.style.borderColor = 'red';
+  if (!isValid) {
+    message.textContent = 'Please fill out all fields.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    return;
+  }
+
+  //Check if Passwords Match
+  if (password1El.value === password2El.value) {
+    passwordsMatch = true;
+    password1El.style.borderColor = 'green';
+    password2El.style.borderColor = 'green';
+  } else {
+    passwordsMatch = false;
+    message.textContent = 'Passwords do not match.';
+    message.style.color = 'red';
+    messageContainer.style.borderColor = 'red';
+    password1El.style.borderColor = 'red';
+    password2El.style.borderColor = 'red';
+    return;
+  }
+
+  // If form is valid and passowrds match
+  if (isValid && passwordsMatch) {
+    message.textContent = 'Successfully Registered!';
+    message.style.color = 'green';
+    messageContainer.style.borderColor = 'green';
+  }
+}
+
+// Store Form Data
+function storeFormData() {
+  const user = {
+    name: form.name.value,
+    phone: form.phone.value,
+    email: form.email.value,
+    website: form.website.value,
+    password: form.password.value,
+  };
+  // Do something with user data
+  console.log(user);
 }
 
 // Function Process Form Data
@@ -23,6 +61,11 @@ function processFormData(e) {
   e.preventDefault();
   // Validate form
   validateForm();
+
+  // Submit data if valid {
+  if (isValid && passwordsMatch) {
+    storeFormData();
+  }
 }
 
 // Event Listener
